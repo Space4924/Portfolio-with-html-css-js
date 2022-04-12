29901f8e23a155b3f207457e182bf13d9069ac49
Space4924 <88616921+Space4924@users.noreply.github.com>
2022-04-12 09:59:17 +0530

    Portfolio with html css js uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+
+//         }else{
+//             $('.navbar').removeClass("sticky");
+
+//         }
+//     });
+//     //toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toogleClass("active");
+//         $('.menu-btn i').toogleClass("active");
+//     })
+// // })
+// var typed=new Typed(".typing",
+// {
+//     strings:["Architect","Developer"],
+// typeSpeed:100,
+// backSpeed:80,
+// loop:true
+// });
+// console.log("working in progress")
